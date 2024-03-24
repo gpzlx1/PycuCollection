7716879f5959ac08ffb5cdde46cea1f69dbc1283
Ping Gong <gpzlx1@outlook.com>
2024-03-24 20:27:25 +0800

    use torch_allocator

--- src/cuco_hashmap.cu
+++ src/cuco_hashmap.cu
@@ -5,13 +5,55 @@
 #include "common.h"
 #include "cuco_hashmap.h"
 
+std::unordered_map<unsigned int64_t, torch::Tensor> tensorMemoryPool;
+int64_t temp_size = 0;
+
+template <typename T>
+class torch_allocator {
+ public:
+  using value_type = T;
+
+  torch_allocator() = default;
+
+  template <class U>
+  torch_allocator(torch_allocator<U> const&) noexcept {}
+
+  value_type* allocate(std::size_t n) {
+    int64_t numel =
+        (sizeof(value_type) * n + sizeof(int64_t)) / sizeof(int64_t);
+    torch::Tensor tensor = torch::empty(
+        {numel},
+        torch::TensorOptions().device(torch::kCUDA).dtype(torch::kInt64));
+    value_type* p = reinterpret_cast<value_type*>(tensor.data_ptr<int64_t>());
+    tensorMemoryPool[(unsigned int64_t)p] = tensor;
+    temp_size += tensor.numel() * tensor.element_size();
+    return p;
+  }
+
+  void deallocate(value_type* p, std::size_t) {
+    torch::Tensor tensor = tensorMemoryPool[(unsigned int64_t)p];
+    tensorMemoryPool.erase((unsigned int64_t)p);
+  }
+};
+
+template <typename T, typename U>
+bool operator==(torch_allocator<T> const&, torch_allocator<U> const&) noexcept {
+  return true;
+}
+
+template <typename T, typename U>
+bool operator!=(torch_allocator<T> const& lhs,
+                torch_allocator<U> const& rhs) noexcept {
+  return not(lhs == rhs);
+}
+
 template <typename Key, typename Value>
 class CUCOHashmap : public Hashmap {
  public:
   using map_type = cuco::static_map<
       Key, Value, std::size_t, cuda::thread_scope_device, thrust::equal_to<Key>,
       cuco::linear_probing<4, cuco::default_hash_function<Key>>,
-      cuco::cuda_allocator<cuco::pair<Key, Value>>, cuco::storage<1>>;
+      torch_allocator<cuco::pair<Key, Value>>, cuco::storage<1>>;
 
   CUCOHashmap(torch::Tensor keys, torch::Tensor values, double load_factor) {
     Key constexpr empty_key_sentinel = -1;
@@ -21,6 +63,7 @@ class CUCOHashmap : public Hashmap {
     std::size_t const capacity = std::ceil(numel / load_factor);
 
     // Create a cuco::static_map
+    temp_size = 0;
     map_ = new map_type(capacity, cuco::empty_key{empty_key_sentinel},
                         cuco::empty_value{empty_value_sentinel});
     auto zipped = thrust::make_zip_iterator(
@@ -31,7 +74,7 @@ class CUCOHashmap : public Hashmap {
     key_options_ = keys.options();
     value_options_ = values.options();
     capacity_ = capacity;
-    memory_usage_ = 100;  // for test
+    memory_usage_ = temp_size;  // for test
   };
 
   ~CUCOHashmap() { delete map_; };
